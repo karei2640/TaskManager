62cd50d27df4a5d78b321742ac342f6d31338b58
karei2640 <karei2640@yahoo.co.jp>
2023-05-27 18:07:09 +0900

    TaskManager

---
+++ db/migrate/20230527090211_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :priority
+      t.date :due_date
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
