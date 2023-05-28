43feb7fc65234b94af6768e4a18e5a30ab1c4f8e
karei2640 <karei2640@yahoo.co.jp>
2023-05-28 15:07:46 +0900

    new.indexの作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,44 @@
 class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+    @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to tasks_path, notice: "タスクが作成されました。"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to task_path(@task), notice: "タスクが更新されました。"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path, notice: "タスクが削除されました。"
+    end
+    
+    private
+    
+    def task_params
+    params.require(:task).permit(:title, :description, :priority, :due_date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'tasks#index'
+  
+  resources :tasks, only: [:index, :new, :show, :edit, :create, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
